a7c3813718a1b322ae270cb4a51882c62c8cc9bf
Thomas Carli <thomas.carli@gmail.com>
2021-01-06 17:42:33 +0100

    structure

---
+++ functions/flights/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var escapeHtml = require('escape-html');
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ *                     More info: https://expressjs.com/en/api.html#req
+ * @param {Object} res Cloud Function response context.
+ *                     More info: https://expressjs.com/en/api.html#res
+ */
+exports.helloHttp = function (req, res) {
+    res.send("Hello " + escapeHtml(req.query.name || req.body.name || 'World') + "!");
+};
